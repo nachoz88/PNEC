dbec912d4a87f856c7aa655503b3a4b175829c31
Nicholas Kanyagia <nkk77707@gmail.com>
2016-06-23 06:22:15 +0000

    before gandolf

---
+++ app/mailers/receipt_mailer.rb
@@ -0,0 +1,12 @@
+class ReceiptMailer < ApplicationMailer
+    default from: 'PNEC Enterprises'
+ 
+  def receipt_email(email, total, order_id)
+   @email=email
+   @total=total
+   @order_id=order_id
+   attachments.inline['pnec.png'] = File.read('public/images/pnec.png')
+    mail(to: email , subject: 'Payment Successfull')
+
+  end
+end
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LddSiMTAAAAAINEs78IxkCw3DDUTJN6OagCkHnm'
+  config.private_key = '6LddSiMTAAAAAMRTy5NzVbV7lnqLSW7UyK06n9NI'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
