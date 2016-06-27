b6df88aa3b3175fa55f7bee11dd739d52c8bd564
Nicholas Kanyagia <nkk77707@gmail.com>
2016-06-27 12:13:59 +0000

    stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   
-  config.action_mailer.default_url_options = { :host => 'pnec-nachoz88.c9users.io' }
+  config.action_mailer.default_url_options = { :host => '52.40.173.238' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
